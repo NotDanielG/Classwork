abd41eeed51a157ea798b2321c820ffe68dcf7fd
Student 6 <Student 6@10.8.33.163>
2016-09-16 12:31:39 -0400

    Added 3 new classes.

---
+++ DanielIntroduction/src/introUnit/OOPExample.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package introUnit;
+
+/**
+ * @author Student 6
+ *
+ */
+public class OOPExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Student jillian = new Senior("Jillian");
+		Student joseph = new Student("Joseph");
+		Student jordan = new Student("Jordan");
+		jillian.talk();
+		joseph.talk();
+		jordan.talk();
+
+	}
+
+}
