6d3f5ac2b946e8e5a234cb657599f5917f55c79b
Student 6 <Student 6@10.8.33.163>
2016-09-21 13:09:15 -0400

    StringText

--- DanielIntroduction/src/datatypes6/StringPractice.java
+++ DanielIntroduction/src/datatypes6/StringPractice.java
@@ -3,7 +3,11 @@ package datatypes6;
 public class StringPractice {
 
 	public static void main(String[] args) {
-		String text = new String("Hello world");
+//		String text = new String("Hello world");
+		
+		String text1 = "Hello World";
+		String text2 = "Hello ";
+		String text3 = "World";
 		
 		System.out.println(text);
 
