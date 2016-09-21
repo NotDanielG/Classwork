ce2786efc30ee7040a426d6e87d7f2f324292c4e
Daniel2 <dgoon9608>
2016-09-21 19:17:20 -0400

    Pick a random card.

--- DanielIntroduction/src/datatypes6/StringPractice.java
+++ DanielIntroduction/src/datatypes6/StringPractice.java
@@ -4,15 +4,29 @@ public class StringPractice {
 
 	public static void main(String[] args) {
 //		String text = new String("Hello world");
+//		
+//		String text1 = "Hello World";
+//		String text2 = "Hello ";
+//		String text3 = "World";
+//		
+//		if(text1.equals(text2+text3)){
+//			System.out.println("These strings are equal.");
+//		}
+		String[] numbers = {"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace"};
+		String[] suits = {"Diamond","Clubs","Hearts","Spades"};
 		
-		String text1 = "Hello World";
-		String text2 = "Hello ";
-		String text3 = "World";
-		
-		if(text1.equals(text2+text3)){
-			System.out.println("These strings are equal.");
+		for(int i = 0; i < 100; i++){
+			System.out.println(numbers[PickNumber(numbers.length)-1] + " of " + suits[PickSuit(suits.length)-1]);
 		}
-
 	}
-
+	public static int PickNumber(int par){
+		double rand = Math.random();
+		int result = (int)(rand*par);
+		return result+1;
+	}
+	
+	public static int PickSuit(int par){
+		return PickNumber(par);
+	}
 }
+
