ee80c16a7e8ef6f2891fa5a85a36697afc33734d
Student 6 <Student 6@10.8.33.163>
2016-09-21 13:07:26 -0400

    Fix

--- DanielIntroduction/src/datatypes6/MathRandom.java
+++ DanielIntroduction/src/datatypes6/MathRandom.java
@@ -4,18 +4,34 @@ import java.util.Random;
 
 public class MathRandom {
 
-	public static void main(String[] args) {
-		for(int index = 0; index<10;index++){
-			System.out.println("Roll #"+(index+1)+" Die is "+rollFairDie());
-		}
-
-	}
+//	public static void main(String[] args){
+//		int totalRolls = 10000;
+//		int[] results = new int[6];
+//		for(int index = 0; index<totalRolls;index++){
+//			int roll = rollUnfairDie();
+//			System.out.println("Roll #"+(index+1)+" Die is "+ roll);
+//			results[roll-1]++;
+//		}
+//		for(int i = 0; i < results.length; i++){
+//			System.out.println((i+1) + " was rolled "+(double)100*results[i]/totalRolls+"% of the time.");
+//		}
+//
+//	}
 	public static int rollFairDie(){
-		Random gen = new Random();
-		return gen.nextInt(6)+1;
+//		Random gen = new Random();
+//		return gen.nextInt(6)+1;
+		double rand = Math.random();//double between (0,1)
+		int roll = (int)(6*rand);
+		return roll+1;
 	}
 	public static int rollUnfairDie(){
-		Random gen = new Random();
-		return gen.nextInt(6)+1;
+		int roll = rollFairDie();
+		if (roll == 1){
+			return rollFairDie();
+		}
+		else{
+			return roll;
+		}
+		
 	}
 }
---
+++ DanielIntroduction/src/datatypes6/StringPractice.java
@@ -0,0 +1,12 @@
+package datatypes6;
+
+public class StringPractice {
+
+	public static void main(String[] args) {
+		String text = new String("Hello world");
+		
+		System.out.println(text);
+
+	}
+
+}
