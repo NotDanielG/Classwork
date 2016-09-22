a8083bcfda745742b6c0e4f3d98e397045bd7095
Student 6 <Student 6@10.8.33.163>
2016-09-22 12:20:45 -0400

    Console Typing.

---
+++ DanielIntroduction/src/datatypes6/RandomCard.java
@@ -0,0 +1,22 @@
+package datatypes6;
+
+public class RandomCard {
+
+	public static void main(String[] args) {
+		String[] numbers = {"Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace"};
+		String[] suits = {"Diamond","Clubs","Hearts","Spades"};
+		
+		for(int i = 0; i < 100; i++){
+			System.out.println(numbers[PickNumber(numbers.length)-1] + " of " + suits[PickSuit(suits.length)-1]);
+		}
+	}
+	public static int PickNumber(int par){
+		double rand = Math.random();
+		int result = (int)(rand*par);
+		return result+1;
+	}
+		
+	public static int PickSuit(int par){
+			return PickNumber(par);
+	}
+}
--- DanielIntroduction/src/datatypes6/StringPractice.java
+++ DanielIntroduction/src/datatypes6/StringPractice.java
@@ -1,32 +1,50 @@
 package datatypes6;
 
+import java.util.Scanner;
+
 public class StringPractice {
 
+	static Scanner input;
+	static String user;
+	
 	public static void main(String[] args) {
-//		String text = new String("Hello world");
-//		
-//		String text1 = "Hello World";
-//		String text2 = "Hello ";
-//		String text3 = "World";
-//		
-//		if(text1.equals(text2+text3)){
-//			System.out.println("These strings are equal.");
-//		}
-		String[] numbers = {"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace"};
-		String[] suits = {"Diamond","Clubs","Hearts","Spades"};
-		
-		for(int i = 0; i < 100; i++){
-			System.out.println(numbers[PickNumber(numbers.length)-1] + " of " + suits[PickSuit(suits.length)-1]);
+		createAScanner();
+		//demonstrateStringMethod();
+		promptName();
+		talkForever();
+	}
+	private static void promptName() {
+		print("Hello, human. I am a board covered with "
+				+ "semiconductors and other such electronic components. "
+				+ "What is your name?");
+		user = input.nextLine();
+		print("Awesome, I will call you "+user+" until you terminate me.");
+	}
+	public static void talkForever(){
+		while(true){
+			promptInput();
 		}
 	}
-	public static int PickNumber(int par){
-		double rand = Math.random();
-		int result = (int)(rand*par);
-		return result+1;
+	public static void promptInput() {
+		print(user +" ,try inputing a String!");
+		String userInput = input.nextLine();
+		print("You typed: " + userInput);
 	}
-	
-	public static int PickSuit(int par){
-		return PickNumber(par);
+	public static void print (String s){
+		System.out.println(s);
+	}
+	private static void createAScanner() {
+		input = new Scanner(System.in);
+		
+	}
+	public static void demonstrateStringMethod(){
+		String text1 = "Hello World";
+		String text2 = "Hello ";
+		String text3 = "World";
+		
+		if(text1.equals(text2+text3)){
+			System.out.println("These strings are equal.");
+		}
 	}
 }
 
