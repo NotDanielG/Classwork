b503f2a113fb159dd301d06d8c87a77375fbbfc4
Daniel2 <dgoon9608>
2016-10-17 21:05:20 -0400

    Fix

--- DanielIntroduction/src/datatypes6/StringPractice.java
+++ DanielIntroduction/src/datatypes6/StringPractice.java
@@ -1,32 +1,83 @@
 package datatypes6;
 
-public class StringPractice {
+import java.util.Scanner;
 
+public class StringPractice {
+	static Scanner input;
+	static String user;
+	static int lineCount;
+	
 	public static void main(String[] args) {
-//		String text = new String("Hello world");
-//		
-//		String text1 = "Hello World";
-//		String text2 = "Hello ";
-//		String text3 = "World";
-//		
-//		if(text1.equals(text2+text3)){
-//			System.out.println("These strings are equal.");
-//		}
-		String[] numbers = {"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace"};
-		String[] suits = {"Diamond","Clubs","Hearts","Spades"};
-		
-		for(int i = 0; i < 100; i++){
-			System.out.println(numbers[PickNumber(numbers.length)-1] + " of " + suits[PickSuit(suits.length)-1]);
+		createAScanner();
+		lineCount = 0;
+		//demonstrateStringMethod();
+		promptName();
+		talkForever();
+	}
+	public static String getInput(){
+		return input.nextLine();
+	}
+	private static void promptName() {
+		print("Hello, human. I am a board covered with "
+				+ "semiconductors and other such electronic components. "
+				+ "What is your name?");
+		user = input.nextLine();
+		print("Awesome, I will call you "+user+" until you terminate me.");
+	}
+	public static void talkForever(){
+		boolean inLoop = true;
+		while(inLoop){
+//			promptInput();
+			print("Greetings, " + user +" How are you?");
+			String response = getInput();
+			if(response.indexOf("good")>= 0){
+				print("I'm so happy you are good!");
+			}
+			else{
+				print("I'm sorry, I don't understand you.");
+			}
 		}
 	}
-	public static int PickNumber(int par){
-		double rand = Math.random();
-		int result = (int)(rand*par);
-		return result+1;
+	public static void promptInput() {
+		print(user +" ,try inputing a String!");
+		String userInput = input.nextLine();
+		print("You typed: " + userInput);
 	}
-	
-	public static int PickSuit(int par){
-		return PickNumber(par);
+	public static void print (String s){
+//		lineCount++;
+		
+		String printString = "";
+		int cutOff = 35;
+		while(s.length() > 0){
+			String currentLine = "";
+			String nextWord = "";
+			//while the currentLine and nextWord are less than the cutioff. Do the following loop. ass
+			while(currentLine.length() +nextWord.length() <= cutOff && s.length() > 0){
+				currentLine += nextWord;
+				s = s.substring(nextWord.length());
+				int endOfWord = s.indexOf(" ");
+				if(endOfWord == -1){
+					endOfWord = s.length()-1;
+				}
+				nextWord = s.substring(0, endOfWord+1);
+			}
+			printString += currentLine +"\n";
+		}
+		
+		System.out.println(printString);
+	}
+	private static void createAScanner() {
+		input = new Scanner(System.in);
+		
+	}
+	public static void demonstrateStringMethod(){
+		String text1 = "Hello World";
+		String text2 = "Hello ";
+		String text3 = "World";
+		
+		if(text1.equals(text2+text3)){
+			System.out.println("These strings are equal.");
+		}
 	}
 }
 
