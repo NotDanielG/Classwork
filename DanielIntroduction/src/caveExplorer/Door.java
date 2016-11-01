ee5721d23540ea4364b0e66e7b45965ea98df2be
Student 6 <Student 6@10.8.33.163>
2016-11-01 12:28:14 -0400

    D

--- DanielIntroduction/src/caveExplorer/Door.java
+++ DanielIntroduction/src/caveExplorer/Door.java
@@ -4,6 +4,7 @@ public class Door {
 	
 	private boolean locked;
 	private boolean open;
+	private String description;
 	public String getDescription() {
 		// TODO Auto-generated method stub
 		return null;
