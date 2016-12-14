f724240e2d81f1503fc8b08f9c3dfc49c5e7a58c
Student 6 <Student 6@10.8.33.163>
2016-12-14 13:17:01 -0500

    Test

--- DanielIntroduction/src/gui/components/TextLabel.java
+++ DanielIntroduction/src/gui/components/TextLabel.java
@@ -25,7 +25,7 @@ public class TextLabel extends Components {
 		g.setColor(Color.black);
 		g.setFont(new Font(font,Font.PLAIN,size));
 		if(text!=null){
-			g.drawString(this.getText(), getx()+getWidth()/4, gety()+getHeight()/3);
+			g.drawString(this.getText(), getWidth()/3, gety()+getHeight()/3);
 		}
 	}
 
