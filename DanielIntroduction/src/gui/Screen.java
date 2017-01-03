a4b89c7fc09ca9620b5ad5fa7516ed426f6731c8
Student 6 <Student 6@10.8.33.163>
2017-01-03 12:32:28 -0500

    Works

--- DanielIntroduction/src/gui/Screen.java
+++ DanielIntroduction/src/gui/Screen.java
@@ -39,7 +39,8 @@ public abstract class Screen {
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setColor(Color.white);
 		g.fillRect(0,0,image.getWidth(),image.getHeight());
-		for(Visible v: viewObjects){
+		for(int i = 0; i < viewObjects.size();i++){
+			Visible v = viewObjects.get(i);
 			g.drawImage(v.getImage(), v.getx(), v.gety(),null);
 		}
 //		g.setColor(Color.black);
@@ -55,12 +56,49 @@ public abstract class Screen {
 		return null;
 	}
 	public void addObject(Visible v) {
-		// TODO Auto-generated method stub
+		viewObjects.add(v);
 		
 	}
 	public void remove(Visible v) {
-		// TODO Auto-generated method stub
+		//In this implementation, we have a very simple command: remove(v)
+		//When an object is removed from a list, every other object after that object is moved up in order
+		//therefore, all of there respective indices change, you must be aware of this
 		
+		/**
+		 * Following is an example that is wrong:
+		 * suppose you have a List<Integer> with
+		 * 4,8,7,1
+		 * Suppose you want to remove all integers greater than 5, you do this
+		 * for(int i = 0; i < list.size(); i++){
+		 * 		if(list.get(i) > 5) list.remove(i);
+		 * 		You fail.
+		 * }
+		 * i = 0, NC
+		 * i = 1, 8 removed: 4,7,1
+		 * i = 2, NC
+		 * i = 3, Out of Loop: 4,7,1 
+		 * 
+		 * Correct Solutions
+		 * 		while(list.get(i) > 5) list.remove(i);
+		 * 		or by adding a i--;
+		 * 
+		 */
+		viewObjects.remove(v);
+		
+	}
+	public void moveToBack(Visible v){
+		if(viewObjects.contains(v)){
+			viewObjects.remove(v);
+			viewObjects.add(0,v);
+			
+		}
+	}
+	public void moveToFront(Visible v){
+		if(viewObjects.contains(v)){
+			viewObjects.remove(v);
+			viewObjects.add(v);
+			
+		}
 	}
 
 }
