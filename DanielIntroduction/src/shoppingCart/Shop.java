18e19315cc8df1401eb4457324468d829c142354
Student 6 <Student 6@10.8.33.175>
2017-03-06 13:02:21 -0500

    Test

--- DanielIntroduction/src/shoppingCart/Shop.java
+++ DanielIntroduction/src/shoppingCart/Shop.java
@@ -1,7 +1,39 @@
 package shoppingCart;
 
+import java.util.ArrayList;
+
 public class Shop {
+	static ArrayList<Item> shoppingList;
 	public static void main(String[] args){
+		
+		String[] justDescriptions = getDescriptions();
+		initList(justDescriptions);
+		doShopping();
+		printPurchasedItem();
+	}
+	private static String[] getDescriptions() {
+		String[] test = {"Eggs","Dee","Whale"};
+		return test;
+	}
+	private static void printPurchasedItem() {
+		for(int i = 0; i < shoppingList.size();i++){
+			if(shoppingList.get(i).getPurc()){
+				System.out.println(shoppingList.get(i).getDesc());
+				shoppingList.remove(i);
+				i--;
+			}
+		}
+	}
+	private static void doShopping() {
+		shoppingList.get(0).setPurchase(true);
+		shoppingList.get(1).setPurchase(true);
+		
+	}
+	private static void initList(String[] justDescriptions) {
+		shoppingList = new ArrayList<Item>();
+		for(int i = 0; i < justDescriptions.length;i++){
+			shoppingList.add(new Item(justDescriptions[i]));
+		}
 		
 	}
 }
